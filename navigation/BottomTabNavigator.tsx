45f3345819a4f2d45efcc641e8ed8427be1ec1b4
John Ratana <jratana1@gmail.com>
2021-05-13 12:16:19 -0400

    removed comments

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -55,11 +55,7 @@ export default function BottomTabNavigator() {
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
 function TabBarIcon(props: { type: string; name: string; color: string}) {
-
-// function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string}) {
-  // return <Ionicons size={24} style={{ marginBottom: -3 }} {...props} />;
 return <Icon size={24} style={{ marginBottom: -3 }} {...props}/>
-
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
