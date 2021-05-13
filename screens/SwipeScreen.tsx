031a63f932e84a07cec8745bfea504468ec535c2
John Ratana <jratana1@gmail.com>
2021-05-13 15:31:07 -0400

    cant render card props

--- screens/SwipeScreen.tsx
+++ screens/SwipeScreen.tsx
@@ -3,13 +3,63 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import Card from '../components/Card'
+import {useState, useEffect} from 'react'
 
 export default function SwipeScreen() {
+const [restList, setRestList] =useState([])
+
+useEffect(
+    () => {fetch('https://api.yelp.com/v3/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer w6qa8__4fH9T6fuiTpxA09hBfrKhosMvhe9N4EVtpZ6GaqJpTTasxnDkgApBCtGUGbiO9VinV1x4nU9VhVeMLepRZa1CZdHpK-o33NtvPj2LsFag44iGgPMqkx6eX3Yx`
+      },
+      body: JSON.stringify({
+        query: `
+            query GetRest{
+      search(term: "burrito", location: "philly") {
+        business {
+          id
+          name
+          display_phone
+          location {
+            address1
+            address2
+            address3
+            city
+            state
+            postal_code
+            formatted_address
+          }
+
+          categories {
+            title
+            alias
+          }
+          photos
+        }
+      }
+    }`
+    
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+            setRestList(result.data.search.business)
+            console.log(restList)
+    
+        })}
+      , []
+    )
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Testing</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <EditScreenInfo path="/screens/SwipeScreen.tsx" />
+      {/* {restList && <Card pic={restList[0].photos[0]} title={restList[0].name} caption={restList[0].location.address1}></Card>} */}
     </View>
   );
 }
