2ddfaee4a779a8c5b0dc6bd8b5c8ea2916cbe41e
John Ratana <jratana1@gmail.com>
2021-06-11 21:39:03 -0400

    update

--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -6,6 +6,8 @@ import { Text, View } from '../components/Themed';
 import {useState, useEffect, useCallback} from 'react'
 import Card from '../components/Card'
 
+import { Tile } from 'react-native-elements'
+
 const BASE_URL = "http://cd2ed39be702.ngrok.io";
 
 
@@ -48,6 +50,37 @@ export default function ListScreen() {
       })
     }
   }
+
+    //   return (
+    //   <SafeAreaView>
+    //     <ScrollView>
+    //       <Text h2 h2Style={styles.h2Style}>
+    //         Top Picks
+    //       </Text>
+    //       <Text h4 h4Style={styles.h4Style}>
+    //         Featured profiles of the day, picked just for you
+    //       </Text>
+    //       <View style={styles.grid}>
+    //         {userRestArray.map((rest: any, index: number) => (
+    //           <Tile
+    //             imageSrc={{uri: rest.photos[0]}}
+    //             activeOpacity={1.0}
+    //             title={rest.name}
+    //             titleStyle={styles.title}
+    //             caption={<View style={{backgroundColor: 'rgba(52, 52, 52, 0.0)', position: 'absolute', left: 10, bottom: 10}}>
+    //                       <Text style={styles.text}>{rest.location.address1}</Text>
+    //                       <Text style={styles.text}>{rest.location.city}, {rest.location.state} {rest.location.postal_code}</Text>
+    //                     </View>}
+    //             captionStyle={styles.caption}
+    //             featured
+    //             key={rest.name}
+    //           />
+    //         ))}
+    //       </View>
+    //     </ScrollView>
+    //   </SafeAreaView>
+    // )
+
   return (
     <SafeAreaView>
     <ScrollView>
@@ -59,20 +92,61 @@ export default function ListScreen() {
 
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  h2Style: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#000000',
+  },
+  h4Style: {
+    textAlign: 'center',
+    color: '#757575',
+  },
+  grid: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  title: {
+    position: 'absolute',
+    left: 10,
+    bottom: 50,
+    backgroundColor: 'black',
+    marginBottom: -2,
+    padding: 10,
+  },
+  caption: {
+    position: 'absolute',
+    left: 10,
+    bottom: 0,
+    backgroundColor: 'black',
+    marginTop: 10,
+    padding: 10,
+  },
+    container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
+  text:{
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+    fontSize: 14,
+    color: "white"
+  }
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
